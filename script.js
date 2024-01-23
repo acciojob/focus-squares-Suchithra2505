d7cb3f397a51678987b6639e241ca58387fe0035
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-01-23 17:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+  const squares = document.querySelectorAll('.square');
+
+  function changeColor(event) {
+    squares.forEach(square => {
+      square.style.backgroundColor = '#6F4E37'; // Change color to Coffee
+    });
+
+    const currentSquare = event.target;
+    currentSquare.style.backgroundColor = '#E6E6FA'; // Keep the current square lavender
+  }
+
+  squares.forEach(square => {
+    square.addEventListener('mouseover', changeColor);
+  });
+});
